eb8c8da8048e82d6a1f6328cc1577f28ab56eee3
Justin Lui <jlui@surveymonkey.com>
2020-03-18 12:42:17 -0700

    changes needed to all esc to close

--- src/newVisitorDialog.js
+++ src/newVisitorDialog.js
@@ -1,20 +1,25 @@
-import React, { useEffect, useRef } from 'react';
-import './visitorDialog.scss';
-import dialogPolyfill from 'dialog-polyfill';
-import PropTypes from 'prop-types';
+import React, { useEffect, useRef } from "react";
+import "./visitorDialog.scss";
+import dialogPolyfill from "dialog-polyfill";
+import PropTypes from "prop-types";
 
 const NewVisitorDialog = ({ show, setShowDialog, addNewVisitor }) => {
   const dialog = useRef(null);
 
   useEffect(() => {
     dialogPolyfill.registerDialog(dialog.current);
-    dialog.current.addEventListener('close', () => {
+    dialog.current.addEventListener("close", () => {
       // need to update the parent container state representation of this dialog.
       // otherwise this will cause a bug where clicking the same movie after closing
       // dialog won't open the dialog
       setShowDialog(false);
     });
   }, [setShowDialog]);
+
+  // this check is needed in Firefox that is using the polyfill
+  if (show && dialog.current && !dialog.current.hasAttribute("open")) {
+    dialog.current.showModal();
+  }
 
   const closeModal = () => {
     dialog.current.close();
@@ -24,26 +29,26 @@ const NewVisitorDialog = ({ show, setShowDialog, addNewVisitor }) => {
     evt.preventDefault();
     const inputs = Array.prototype.filter.call(
       evt.target.elements,
-      node => node.tagName.toLowerCase() === 'input'
+      node => node.tagName.toLowerCase() === "input"
     );
 
     const name = inputs
       .reduce((acc, node) => {
-        if (node.placeholder.includes('name')) {
+        if (node.placeholder.includes("name")) {
           return acc.push(node.value) && acc;
         } else {
           return acc;
         }
       }, [])
-      .join(' ');
+      .join(" ");
 
     const notes = inputs.reduce((acc, node) => {
-      if (node.placeholder === 'notes') {
+      if (node.placeholder === "notes") {
         return (acc += node.value);
       } else {
         return acc;
       }
-    }, '');
+    }, "");
 
     if (inputs.every(node => !!node.value.trim())) {
       addNewVisitor({ name, notes }).then(() => {
@@ -53,12 +58,13 @@ const NewVisitorDialog = ({ show, setShowDialog, addNewVisitor }) => {
   };
 
   return (
-    <dialog ref={dialog} open={show}>
+    <dialog ref={dialog}>
       <i
         role="button"
         aria-hidden="true"
         className="close-button fas fa-times-circle"
         onClick={closeModal}
+        autoFocus
       />
       <h1>Please enter visitor details</h1>
       <form onSubmit={handleSubmit}>
@@ -67,7 +73,7 @@ const NewVisitorDialog = ({ show, setShowDialog, addNewVisitor }) => {
           <input placeholder="Last name" />
         </p>
         <p>
-        <input placeholder="Notes" />
+          <input placeholder="Notes" />
         </p>
         <button type="submit">Save</button>
       </form>
