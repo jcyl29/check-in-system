5e6cdb652097896a50a7e8dfa92b4e3599b83020
Justin Lui <jlui@surveymonkey.com>
2020-03-19 03:08:16 -0700

    more tests

---
+++ src/useDebounce.test.js
@@ -0,0 +1,33 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useDebounce from './useDebounce';
+
+jest.useFakeTimers();
+
+describe('useDebounce', () => {
+  test('should retain the initial value it set', () => {
+    const { result } = renderHook(
+      ({ initialValue }) => useDebounce(initialValue, 500),
+      {
+        initialProps: { initialValue: 'abc' }
+      }
+    );
+
+    expect(result.current).toBe('abc');
+  });
+
+  test('should update with new value after timeout has passed', () => {
+    const { result, rerender } = renderHook(
+      ({ initialValue }) => useDebounce(initialValue, 500),
+      {
+        initialProps: { initialValue: 'abc' }
+      }
+    );
+    
+    rerender({ initialValue: 'def' });
+    expect(result.current).toBe('abc');
+    act(() => {
+      jest.runAllTimers();
+      expect(result.current).toBe('def');
+    });
+  });
+});
